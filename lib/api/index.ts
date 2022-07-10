324353cc30fe70ee56c6bcc6a6de13e202f19be3
lepropst <propstle@gmail.com>
2022-07-10 13:03:45 -0500

    small error in imports

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -1,7 +1,1 @@
-import Api from "./api";
-export * from "./api";
-// export function useApi(directory: string) {
-//   const api = Api(directory);
-//   const content = api.getAll();
-//   return content;
-// }
+
