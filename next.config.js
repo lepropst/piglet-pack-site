1db42f9e7373e65ee420ab46d0da2a2c40a525a5
Elias Rangel <propstle@gmail.com>
2022-07-16 08:24:54 -0500

    channged sitemap

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./scripts/generate-sitemap');
-    }
 
-    return config;
-  },
   async redirects() {
     return [
       {
