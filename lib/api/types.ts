560fe877390a2d55027ada78455027058d0a73df
Elias Rangel <propstle@gmail.com>
2022-10-05 11:43:15 -0500

    fixedbuild

--- lib/api/types.ts
+++ lib/api/types.ts
@@ -1,10 +1,5 @@
-import {
-  TypePage_help_center_articleFields,
-  TypePage_landingFields,
-} from './generated-types';
+import { TypeBlogPostFields, TypeAuthorFields } from './generated-types';
 import * as Contentful from 'contentful';
 
 export * from './generated-types';
-export type TypePage = Contentful.Entry<
-  TypePage_help_center_articleFields | TypePage_landingFields
->;
+export type TypePage = Contentful.Entry<TypeBlogPostFields | TypeAuthorFields>;
